45d771e6d2dd109062de93c09b1a0e2e403cd24f
Berhudan Başcan <78546530+SiliconValley-Star@users.noreply.github.com>
2026-01-20 09:00:25 +0300

    Import path düzeltmesi

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { registerServiceWorker } from './utils/serviceWorker';
+import { registerServiceWorker } from './src/utils/serviceWorker';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
